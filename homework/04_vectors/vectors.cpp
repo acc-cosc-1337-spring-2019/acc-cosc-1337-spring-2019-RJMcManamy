916092bd484f2cb1e0d990c43d93c9d6beee6c8f
8385218 <robert.mcmanamy@g.austincc.edu>
2019-02-19 19:30:15 -0600

    working on homework 4

--- homework/04_vectors/vectors.cpp
+++ homework/04_vectors/vectors.cpp
@@ -1,3 +1,4 @@
+#include "vectors.h"
 /*
 Write a value return function get_max_from_vector with a const reference 
 vector of intsparameter that returns the max value in a vector
@@ -5,8 +6,19 @@ vector of intsparameter that returns the max value in a vector
 @param numbers is a const reference to a vector of integers
 @return the max value in the vector
 */
-
-
+int get_max_from_vector(const vector<int>& values)
+{
+	int max = 0;
+	for (int i = 0; i < values.size(); i++)
+	{
+		if (values[i] > max)
+		{
+			max = values[i];
+		}
+		
+	}
+	return max;
+}
 
 /*
 Write a function named is_prime with an integer parameter that
@@ -15,6 +27,19 @@ given a number returns true if prime or false if not prime
 @param number: Any whole number
 @return: bool if prime False if not
 */
+bool is_prime(int num)
+{
+	bool prime = true;
+	for (int i = 2; i <= num / 2; ++i)
+	{
+		if (num % i == 0)
+		{
+			prime = false;
+			break;
+		}
+	}
+	return prime;
+}
 
 /*
 Write a a function named vector_of_primes with an integer parameter
@@ -28,3 +53,17 @@ Example given number 10 returns a vector with elements 2,3,5,7,
 Make sure to use the is_prime function to determine if current 
 number is prime.
 */
+vector<int> vector_of_primes(int number)
+{
+	vector<int> primes = {};
+
+	for (int i = 1; i < number; ++i)
+	{
+		if (is_prime(i))
+		{
+			primes.push_back(i);
+		}
+	}
+	return primes;
+}
+
--- homework/04_vectors_test/vectors_test.cpp
+++ homework/04_vectors_test/vectors_test.cpp
@@ -1,5 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "vectors.h"
 
 
 TEST_CASE("Verify Test Configuration", "verification") {
@@ -12,7 +13,15 @@ vector values 3,8,1,99,1000 returns 1000
 vector values 15,12,11,99,88 returns 99
 vector values 150,120,11,990,88888 returns 88888
 */
-
+TEST_CASE("Test get max from vector function")
+{
+	vector<int> values1 = { 3, 8, 1, 99, 1000 };
+	vector<int> values2 = { 15, 12, 11, 99, 88 };
+	vector<int> values3 = { 150, 120, 11, 990, 88888 };
+	REQUIRE(get_max_from_vector(values1) == 1000);
+	REQUIRE(get_max_from_vector(values2) == 99);
+	REQUIRE(get_max_from_vector(values3) == 88888);
+}
 
 /*
 Write a test case for is_prime with values:
@@ -21,6 +30,13 @@ Write a test case for is_prime with values:
 43 returns true
 44 returns false
 */
+TEST_CASE("Test is prime function")
+{
+	REQUIRE(is_prime(2) == true);
+	REQUIRE(is_prime(4) == false);
+	REQUIRE(is_prime(43) == true);
+	REQUIRE(is_prime(44) == false);
+}
 
 
 /*
@@ -30,3 +46,10 @@ argument 10 return vector should have the following items:
 argument 50 return vector should have the following items:
 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47
 */
+TEST_CASE("Test vector of primes function")
+{
+	vector<int> set1 = { 2, 3, 5, 7 };
+	vector<int> set2 = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 };
+	REQUIRE(vector_of_primes(10) == set1);
+	REQUIRE(vector_of_primes(50) == set2);
+}
